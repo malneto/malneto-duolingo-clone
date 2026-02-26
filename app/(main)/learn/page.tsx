8aa80140f0ce0c680c7c834ffdf7c2b941728115
MARIO <malneto@gmail.com>
2026-02-25 22:40:57 -0300

    Atualiza layout da página Learn com top bar e bottom navigation

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -56,7 +56,8 @@ const LearnPage = async () => {
         hasActiveSubscription={isPro}
       />
 
-      <div className="flex flex-row-reverse gap-[48px] px-6 pt-16 pb-20 lg:pb-0">
+      {/* CONTEÚDO PRINCIPAL */}
+      <div className="flex flex-row-reverse gap-[48px] px-6 pt-16 pb-24 lg:pb-0 min-h-screen">
         
         <StickyWrapper>
           <UserProgress
@@ -78,8 +79,7 @@ const LearnPage = async () => {
         </StickyWrapper>
 
         <FeedWrapper>
-          
-          {/*<Header title={userProgress.activeCourse.title} />*/}
+          <Header title={userProgress.activeCourse.title} />
           
           {units.map((unit) => (
             <div key={unit.id} className="mb-10">
@@ -97,7 +97,7 @@ const LearnPage = async () => {
         </FeedWrapper>
       </div>
 
-      {/* MENU INFERIOR FIXO NO CELULAR */}
+      {/* MENU INFERIOR FIXO - SÓ NO CELULAR */}
       <BottomNavigation />
     </>
   );
