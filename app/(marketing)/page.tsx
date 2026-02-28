4c894b6c52cc3bc96aed492b53741b11a25e82df
Mário <malneto@gmail.com>
2026-02-28 16:05:41 -0300

    Landing page Estilo Espaço

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,58 +1,66 @@
-import {
-  ClerkLoaded,
-  ClerkLoading,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-} from "@clerk/nextjs";
-import { Loader } from "lucide-react";
+import { InfinityIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { MESSAGES } from "@/constants/messages";
+import { courses } from "@/db/schema";
 
-export default function MarketingPage() {
+type UserProgressProps = {
+  activeCourse: typeof courses.$inferSelect;
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: UserProgressProps) => {
   return (
-    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
-      <div className="relative mb-8 h-[240px] w-[240px] lg:mb-0 lg:h-[424px] lg:w-[424px]">
-        <Image src="/hero.svg" alt="Hero" fill />
-      </div>
+    <div className="flex w-full items-center justify-between gap-x-2">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            className="rounded-md border"
+            width={32}
+            height={32}
+          />
+        </Button>
+      </Link>
 
-      <div className="flex flex-col items-center gap-y-8">
-        <h1 className="max-w-[480px] text-center text-xl font-bold text-neutral-600 lg:text-3xl">
-          {MESSAGES.marketingTitle}
-        </h1>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            height={28}
+            width={28}
+            alt="Points"
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
 
-        <div className="flex w-full max-w-[330px] flex-col items-center gap-y-3">
-          <ClerkLoading>
-            <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
-          </ClerkLoading>
-
-          <ClerkLoaded>
-            <SignedOut>
-              <SignUpButton mode="modal">
-                <Button size="lg" variant="secondary" className="w-full">
-                  {MESSAGES.getStarted}
-                </Button>
-              </SignUpButton>
-
-              <SignInButton mode="modal">
-                <Button size="lg" variant="primaryOutline" className="w-full">
-                  {MESSAGES.alreadyHaveAccount}
-                </Button>
-              </SignInButton>
-            </SignedOut>
-
-            <SignedIn>
-              <Button size="lg" variant="secondary" className="w-full" asChild>
-                <Link href="/learn">{MESSAGES.continueLearning}</Link>
-              </Button>
-            </SignedIn>
-          </ClerkLoaded>
-        </div>
-      </div>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            height={22}
+            width={22}
+            alt="Hearts"
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="stroke-3 h-4 w-4" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
     </div>
   );
-}
+};
